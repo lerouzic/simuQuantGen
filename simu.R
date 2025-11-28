643d0cfd58a4701405d618202d506a6ebe2d1d90
Arnaud Le Rouzic <lerouzic@egce.cnrs-gif.fr>
2025-11-28 15:38:21 +0100

    Trying to make the interface of crosspopulations more intuitive.

--- simu.R
+++ simu.R
@@ -235,14 +235,18 @@ summary.population <- function(population) {
 	)
 }
 
-clean.inpop <- function(obj) {
+
+clean.inpop <- function(obj, n=1) {
 	# Tries to find a proper population in object obj
 	if (is.list(obj)) {
 		if (is.data.frame(obj)) {
 			pp <- attr(obj, "lastpop")
 			if (is.null(pp)) {
 				stop("Impossible to find a population in the provided object. Have you forgotten the output.pop=TRUE option?")
-			} 
+			}
+                        if (is.list(pp[[1]][[1]])) { # pp is not a population, perhaps a list of populations?
+                                return(pp[[n]])
+                        }
 			return(pp)
 		}
 		return(obj) # Hoping that the list can be interpreted as a population, otherwise the crash will happen later. 
@@ -253,7 +257,7 @@ clean.inpop <- function(obj) {
 
 crosspopulations <- function(
 		pop1, 
-		pop2, 
+		pop2         = NULL, 
 		numcross     = length(pop1), 
 		var.env      = default$var.env,
 		rate.rec     = default$rate.rec,
@@ -263,8 +267,13 @@ crosspopulations <- function(
 		fitness      = default$fitness,
 		output.pop   = FALSE) 
 {
-	pop1 <- clean.inpop(pop1)
-	pop2 <- clean.inpop(pop2)
+        if (!is.null(pop2)) {
+	        pop1 <- clean.inpop(pop1)
+	        pop2 <- clean.inpop(pop2)
+        } else {
+                pop1 <- clean.inpop(pop1, 1)
+                pop2 <- clean.inpop(pop1, 2)
+        }
 	stopifnot(length(pop1) > 0, length(pop2) > 0, 
 	          nrow(pop1[[1]]$genotype) == nrow(pop2[[1]]$genotype))
 	rate.rec <- rep_len(rate.rec, nrow(pop1[[1]]$genotype) - 1)
